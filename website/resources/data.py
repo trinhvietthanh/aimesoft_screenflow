9b386605ba8d2ac3d97cb4cd693c3e2f84e2fc02
Thành Trịnh Việt <thanhtrinhviet09@gmail.com>
2021-02-03 17:32:03 +0700

    fix

--- website/resources/data.py
+++ website/resources/data.py
@@ -12,13 +12,16 @@ from bson.objectid import ObjectId
 import pytz
 # Detail time of Employee
 
+
 @app.template_filter('datetimefilter')
 def datetimefilter(value, format='%H:%M:%S'):
-    tz = pytz.timezone('Asia/Ho_Chi_Minh')  # timezone you want to convert to from UTC (America/Los_Angeles)
+    # timezone you want to convert to from UTC (Asia/Ho_Chi_Minh)
+    tz = pytz.timezone('Asia/Ho_Chi_Minh')
     utc = pytz.timezone('UTC')
-    value = utc.localize(value, is_dst=None).astimezone(pytz.utc)
+    value = utc.localize(value, is_dst=tz).astimezone(pytz.utc)
     local_dt = value.astimezone(tz)
     return local_dt.strftime(format)
+
 
 def ListTimes(times, month, year):
     if(len(times) > 0):
@@ -28,11 +31,12 @@ def ListTimes(times, month, year):
         time_end_am = datetimefilter(time_line['time_end_am'])
         time_start_pm = datetimefilter(time_line['time_start_pm'])
         time_end_pm = datetimefilter(time_line['time_end_pm'])
-
+        checkIn, checkOut= '', ''
         obj = {'id': "", 'checkIn': "", 'checkOut': ""}
         for time in times:
             date = time['time'].strftime("%d/%m/%Y")
-            if(month == time['time'].strftime("%#m") and year == time['time'].strftime("%Y")):
+
+            if(month == time['time'].strftime("%m") and year == time['time'].strftime("%Y")):
 
                 if(obj['id'] == date):
                     if(time['type'] == "checkOut"):
@@ -46,6 +50,7 @@ def ListTimes(times, month, year):
                             obj['checkIn'] = checkIn
 
                 else:
+
                     if(obj['id'] != "" and obj['checkIn'] != "" and obj['checkOut'] != ""):
                         if(time_start_am < obj['checkIn']):
                             checkIn = (obj['checkIn']).split(":")
@@ -97,9 +102,10 @@ def ListTimes(times, month, year):
                     checkOut = (obj['checkOut']).split(":")
                 else:
                     checkOut = (time_end_pm).split(":")
-                if(checkIn and not checkOut and month == time['time'].strftime("%#m")
-                and year == time['time'].strftime("%Y")):
-                    t1 = timedelta(hours=int(checkIn[0]), minutes=int(checkIn[1]))
+                if(checkIn != "" and checkOut != "" and month == time['time'].strftime("%m")
+                   and year == time['time'].strftime("%Y")):
+                    t1 = timedelta(
+                        hours=int(checkIn[0]), minutes=int(checkIn[1]))
                     t2 = timedelta(hours=int(lunch[0]), minutes=int(lunch[1]))
                     t3 = timedelta(
                         hours=int(endlunch[0]), minutes=int(endlunch[1]))
@@ -110,12 +116,13 @@ def ListTimes(times, month, year):
                         obj['total'] = "0:00:00"
                     list_time.append(obj)
             else:
-                
-                if(checkIn and checkOut and month == time['time'].strftime("%#m")
-                and year == time['time'].strftime("%Y")):
+                if(checkIn != "" and checkOut != "" and month == time['time'].strftime("%m")
+                   and year == time['time'].strftime("%Y")):
                     checkOut = (obj['checkOut']).split(":")
-                    t1 = timedelta(hours=int(checkIn[0]), minutes=int(checkIn[1]))
-                    t2 = timedelta(hours=int(checkOut[0]), minutes=int(checkOut[1]))
+                    t1 = timedelta(
+                        hours=int(checkIn[0]), minutes=int(checkIn[1]))
+                    t2 = timedelta(
+                        hours=int(checkOut[0]), minutes=int(checkOut[1]))
                     obj['total'] = str(t2-t1)
                     if (obj['total'] < "0:00:00"):
                         obj['total'] = "0:00:00"
@@ -126,7 +133,7 @@ def ListTimes(times, month, year):
 
 
 class InputApi(Resource):
-    #@jwt_required
+    # @jwt_required
     def post(self):
         body = request.get_json()
         user = Users(**body)
@@ -136,7 +143,7 @@ class InputApi(Resource):
 
 
 class InputApiTime(Resource):
-    #@jwt_required
+    # @jwt_required
     def post(self):
         body = request.get_json()
         time = Trackers(**body)
@@ -147,7 +154,7 @@ class InputApiTime(Resource):
 
 
 class EditEmployee(Resource):
-    #@jwt_required
+    # @jwt_required
     def post(self):
         body = request.get_json()
         user_id = body['_id']
@@ -159,7 +166,7 @@ class EditEmployee(Resource):
 
 
 class OutputEmployee(Resource):
-    #@jwt_required
+    # @jwt_required
     def get(self):
         body = request.args.get('id')
         user = Users.objects.get(pk=body)
@@ -170,13 +177,13 @@ class OutputEmployee(Resource):
 
 
 class OutputApiTime(Resource):
-    #@jwt_required
+    # @jwt_required
     def get(self):
         user = request.args.get('user')
         month = request.args.get('month')
         year = request.args.get('year')
         if(month == None):
-            month = (datetime.now()).strftime("%#m")
+            month = (datetime.now()).strftime("%m")
         if(year == None):
             year = (datetime.now()).strftime("%Y")
         myquery = {"user": {"$oid": user}}
@@ -189,7 +196,7 @@ class OutputApiTime(Resource):
 
 
 class OutputApi(Resource):
-    #@jwt_required
+    # @jwt_required
     def get(self):
         user = Users.objects.all()
         resp = jsonify(user)
@@ -198,7 +205,7 @@ class OutputApi(Resource):
 
 
 class UpdateTimeLine(Resource):
-    #@jwt_required
+    # @jwt_required
     def put(self):
         code = request.args.get('id')
         body = request.get_json()
@@ -209,7 +216,7 @@ class UpdateTimeLine(Resource):
 
 
 class TimeLines(Resource):
-    #@jwt_required
+    # @jwt_required
     def get(self):
         time = TimeLine.objects.first()
         resp = jsonify(time)
